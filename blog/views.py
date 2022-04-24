9eac08da01be33defc2cb5a992957cd7741088ba
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-04-24 19:54:01 +0000

    update views

--- blog/views.py
+++ blog/views.py
@@ -18,10 +18,10 @@ class PostDetail(View):
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
-            liked=True
+            liked = True
 
         return render(
-            request, 
+            request,
             "post_detail.html",
             {
                 "post": post,
